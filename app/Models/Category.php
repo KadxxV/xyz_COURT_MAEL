a4d2cd1589355f713bccad33310b051e0aec35ae
Maël COURT <maelcourt0@gmail.com>
2024-10-25 16:29:08 +0200

    fin du TP

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -17,5 +18,12 @@ class Category extends Model
     public function tracks(): HasMany
     {
         return  $this->hasMany(Track::class);
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->exists ? $this->attributes['name'] : trans('categories.missing_category')
+        );
     }
 }
